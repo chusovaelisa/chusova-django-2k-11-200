025b246c6e71c328eb8b74cdffa0e78e4dd30e8d
Liza <lizzzakkka@mail.ru>
2023-11-28 11:47:09 +0300

    added linter

--- web/forms.py
+++ web/forms.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import get_user_model
 from django import forms
-from .models import Note,  Photo, Category
+from .models import Note, Photo, Category
 
 
 User = get_user_model()
+
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
@@ -11,33 +12,33 @@ class RegistrationForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        if cleaned_data['password'] != cleaned_data['password2']:
-            self.add_error('password', "Пароли не совпадают")
+        if cleaned_data["password"] != cleaned_data["password2"]:
+            self.add_error("password", "Пароли не совпадают")
         return cleaned_data
 
     class Meta:
         model = User
         fields = ("email", "username", "password", "password2")
 
+
 class AuthForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-
-
 class NoteForm(forms.ModelForm):
-    category = forms.ChoiceField(
-        label="Категория", choices=Category.CategoryType.choices,
-        widget=forms.Select(attrs={'class': 'form-control'})
-    )
     class Meta:
         model = Note
-        fields = ['title', 'content', 'category', ]
+        fields = ["title", "content", "category"]
 
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ["category_type"]
 
 
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = ['image', 'caption']
+        fields = ["image", "caption"]
--- web/views.py
+++ web/views.py
@@ -1,31 +1,39 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from datetime import datetime
-from .forms import RegistrationForm, AuthForm, NoteForm, PhotoForm
+from .forms import RegistrationForm, AuthForm, PhotoForm, NoteForm, CategoryForm
 from django.contrib.auth import get_user_model, authenticate, login, logout
-from .models import Note, Photo, Category
+from .models import Photo, Category, Note
 from django.views.generic import ListView
+from PIL import Image
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
+
 
 User = get_user_model()
+
 
 def main_view(request):
     year = datetime.now().year
     return render(request, "web/main.html", {"year": year})
 
+
 def registration_view(request):
     form = RegistrationForm()
     is_success = False
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RegistrationForm(data=request.POST)
         if form.is_valid():
             user = User(
-                username=form.cleaned_data['username'],
-                email=form.cleaned_data['email'],
+                username=form.cleaned_data["username"],
+                email=form.cleaned_data["email"],
             )
-            user.set_password(form.cleaned_data['password'])
+            user.set_password(form.cleaned_data["password"])
             user.save()
             is_success = True
-    return render(request, "web/registration.html", {"form": form, "is_success": is_success})
+    return render(
+        request, "web/registration.html", {"form": form, "is_success": is_success}
+    )
+
 
 def auth_view(request):
     form = AuthForm()
@@ -40,30 +48,70 @@ def auth_view(request):
                 return redirect("main")
     return render(request, "web/auth.html", {"form": form})
 
+
 def logout_view(request):
     logout(request)
     return redirect("main")
 
 
-def create_note_view(request):
-    if request.method == 'POST':
-        form = NoteForm(request.POST)
-        if form.is_valid():
-            note = form.save(commit=False)
-            note.user = request.user
-            note.save()
-            return redirect('list_notes')
+def create_note_view(request, note_id=None):
+    notes = Note.objects.all()
+    categories = Category.objects.all()
+    paginator = Paginator(notes, 10)
+    page = request.GET.get("page")
+
+    try:
+        notes = paginator.page(page)
+    except PageNotAnInteger:
+        notes = paginator.page(1)
+    except EmptyPage:
+        notes = paginator.page(paginator.num_pages)
+
+    category_form = CategoryForm()
+
+    if note_id:
+        note_instance = get_object_or_404(Note, id=note_id)
+        note_form = NoteForm(request.POST or None, instance=note_instance)
     else:
-        form = NoteForm()
+        note_instance = None
+        note_form = NoteForm(request.POST or None, initial={"user": request.user})
 
-    return render(request, 'web/create_note.html', {'form': form})
+    if request.method == "POST":
+        if "add_note" in request.POST:
+            if note_form.is_valid():
+                note_instance = note_form.save(commit=False)
+                note_instance.user = request.user
+                note_instance.save()
 
+                messages.success(request, "Заметка успешно сохранена")
 
-from PIL import Image
+                if note_id:
+                    existing_note = get_object_or_404(Note, id=note_id)
+                    existing_note.delete()  # Удаление существующей заметки
+
+                return redirect("main")
+        elif "add_category" in request.POST:
+            return redirect("create_category")
+
+    return render(
+        request,
+        "web/create_note.html",
+        {
+            "note_form": note_form,
+            "category_form": category_form,
+            "notes": notes,
+            "categories": categories,
+            "user": request.user,
+        },
+    )
+
 
 def create_photo_view(request):
-    if request.method == 'POST':
-        form = PhotoForm(data=request.POST, files=request.FILES, )
+    if request.method == "POST":
+        form = PhotoForm(
+            data=request.POST,
+            files=request.FILES,
+        )
         if form.is_valid():
             photo = form.save(commit=False)
             photo.user = request.user
@@ -71,19 +119,47 @@ def create_photo_view(request):
 
             img = Image.open(photo.image.path)
 
-            return redirect('photo_list')
+            return redirect("photo_list")
 
     else:
         form = PhotoForm()
 
-    return render(request, 'web/create_photo.html', {'form': form})
+    return render(request, "web/create_photo.html", {"form": form})
 
 
 class PhotoListView(ListView):
     model = Photo
-    template_name = 'photo_list.html'
-    context_object_name = 'photos'
+    template_name = "photo_list.html"
+    context_object_name = "photos"
 
-def list_notes_view(request):
-    notes = Note.objects.all()
-    return render(request, 'web/list_notes.html', {'notes': notes})
+
+def create_category_view(request):
+    if request.method == "POST":
+        category_form = CategoryForm(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            return redirect("create_note")
+
+    else:
+        category_form = CategoryForm()
+
+    return render(request, "web/create_category.html", {"category_form": category_form})
+
+
+def edit_note_view(request, note_id):
+    note_instance = get_object_or_404(Note, id=note_id)
+
+    if request.method == "POST":
+        form = NoteForm(request.POST, instance=note_instance)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Заметка успешно изменена")
+            return redirect("main")
+    else:
+        form = NoteForm(instance=note_instance)
+
+    return render(
+        request,
+        "web/edit_note.html",
+        {"note_form": form, "user": request.user, "note_instance": note_instance},
+    )
